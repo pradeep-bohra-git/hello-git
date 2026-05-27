334d2d1cfdff6c24e5c0491cb902121f0c909934
Pradeep Bohra <pradeep.b@amplesemiconductors.com>
2026-05-27 18:28:53 +0530

    Change to test hook again...

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins Pipeline Example...");
+        System.out.println("Hello Jenkins Pipeline Example Tutorial...");
     }
 }
