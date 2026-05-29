00e47fd5790c2bce3e92042cdfd2fe250dd66570
Pradeep Bohra <pradeep.b@amplesemiconductors.com>
2026-05-29 16:15:19 +0530

    Changed file to trigger demo Jenkins build...

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Maven Jenkins Pipeline Example Tutorial...");
+        System.out.println("Ample Semiconductors : Hello Maven Jenkins Pipeline Example Tutorial...");
     }
 }
