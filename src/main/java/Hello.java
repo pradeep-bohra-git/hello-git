b93ef8de2d464f888c7008a1544f34d11e4bc7d2
Pradeep Bohra <pradeep.b@amplesemiconductors.com>
2026-05-29 18:45:55 +0530

    Jenkins build trigger using push...

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Ample Semiconductors Demo Fix : Hello Maven Jenkins Pipeline Example Tutorial...");
+        System.out.println("Ample Semiconductors Fixed Jenkins workspace : Hello Maven Jenkins Pipeline Example Tutorial...");
     }
 }
