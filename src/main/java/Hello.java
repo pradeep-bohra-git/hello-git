5f3508796d5255180d38802d8fe3c9979d1baa12
Pradeep Bohra <pradeep.b@amplesemiconductors.com>
2026-05-29 14:33:09 +0530

    modified file to trigger build...

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins Pipeline Example Tutorial...");
+        System.out.println("Hello Maven Jenkins Pipeline Example Tutorial...");
     }
 }
