0f1eac02cc7c8ed76bc6e4550b4daea62a3bba03
Pradeep Bohra <pradeep.b@amplesemiconductors.com>
2026-06-01 16:35:25 +0530

    json_branch clean up...

--- src/main/java/Hello.java
+++
@@ -1,5 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println(">>> Ample Semiconductors Fixed Jenkins workspace : Hello Maven Jenkins Pipeline Example Tutorial...");
-    }
-}
